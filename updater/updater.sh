c8ddfa69eba6a5ea2fa5e23fabbf3c45b991d847
Dao Hoang Son <daohoangson@gmail.com>
2019-10-17 21:58:24 +0700

    Add `date.txt`

--- updater/updater.sh
+++ updater/updater.sh
@@ -5,11 +5,13 @@ set -e
 _dir=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 
 _gsoPath='data/dvhcvn.json'
+_gsoDatePath='data/date.txt'
 _gisPath='data/gis.json'
 
 if [ ! -f $_gsoPath ]; then
   echo "Generating $_gsoPath..."
   php "$_dir/01_gso.gov.vn.php" >$_gsoPath
+  cat $_gsoPath | jq -r .data_date >$_gsoDatePath
 fi
 
 if [ ! -f $_gisPath ]; then
