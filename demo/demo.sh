b7f9c3a55aa24d74dbc9f91596ae42ddee83aa8d
Dao Hoang Son <daohoangson@gmail.com>
2019-10-18 18:18:35 +0700

    Adjust demo/gis.html for now.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -6,6 +6,5 @@ _dir=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 
 exec docker run --rm \
   -p 80:80 \
-  -v "$_dir:/usr/share/nginx/html" \
-  -v "$_dir/../data:/usr/share/nginx/html/data" \
+  -v "$_dir/..:/usr/share/nginx/html:ro" \
   nginx
