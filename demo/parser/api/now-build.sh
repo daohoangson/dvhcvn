e09e0df4401d14f7afd0ad05735179c6e89b2945
Dao Hoang Son <daohoangson@gmail.com>
2019-10-26 13:04:08 +0700

    Add `now-build` to populate `tree.json` file

---
+++ demo/parser/api/now-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+cd $( dirname $BASH_SOURCE[0] )
+
+_historyDataPath=../../../history/data
+_treeJsonPath=$_historyDataPath/tree.json
+
+mkdir -p $_historyDataPath
+
+if [ ! -f $_treeJsonPath ]; then
+  curl -Lo $_treeJsonPath https://github.com/daohoangson/dvhcvn-historical-data/raw/master/tree.json
+fi
