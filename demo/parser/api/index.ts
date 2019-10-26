20fc2eef435015c2c5f58044e31c6286a9a66468
Dao Hoang Son <daohoangson@gmail.com>
2019-10-26 13:27:13 +0700

    Add demo/parser.html page

--- demo/parser/api/index.ts
+++ demo/parser/api/index.ts
@@ -4,10 +4,15 @@ import Parser from "../src/parser";
 
 const parser = new Parser();
 
-export default function(req: NowRequest, res: NowResponse) {
+export default function (req: NowRequest, res: NowResponse) {
   if (typeof req.body === "string") {
     return res.send(parser.parse(req.body));
-  } else {
-    return res.status(400);
   }
+
+  if (req.body && typeof req.body.input === "string") {
+    const parsed = parser.parse(req.body.input);
+    return res.send(parsed.map(r => `${r.type} ${r.name}`).join(", "))
+  }
+
+  return res.status(400);
 }
