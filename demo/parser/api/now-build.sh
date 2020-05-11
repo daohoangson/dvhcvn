c71c0d2ba0490316392c3132fe242ac978d7d202
Dao Hoang Son <daohoangson@gmail.com>
2020-05-11 10:43:56 +0700

    Fix tree.json download

--- demo/parser/api/now-build.sh
+++ demo/parser/api/now-build.sh
@@ -9,5 +9,7 @@ _treeJsonPath=$_historyDataPath/tree.json
 mkdir -p $_historyDataPath
 
 if [ ! -f $_treeJsonPath ]; then
-  curl -Lo $_treeJsonPath https://github.com/daohoangson/dvhcvn-historical-data/raw/master/tree.json
+  curl -Lo $_treeJsonPath https://github.com/daohoangson/dvhcvn-historical-data/raw/demo/parser/tree.json
 fi
+
+ls -ald $_treeJsonPath
