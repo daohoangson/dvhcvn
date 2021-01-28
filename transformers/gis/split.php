ca30e0ceb2b929fa714f286a3e13e3e3984bb8c6
Đào Hoàng Sơn <daohoangson@gmail.com>
2021-01-28 23:14:51 +0700

    Update 01/02/2021 (#13)

--- transformers/gis/split.php
+++ transformers/gis/split.php
@@ -13,7 +13,9 @@ function main(string $outDir)
         $json = json_encode($level1, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         file_put_contents($path, $json);
 
-        $level1sBbox[$level1['level1_id']] = $level1['bbox'];
+        if (isset($level1['bbox'])) {
+            $level1sBbox[$level1['level1_id']] = $level1['bbox'];
+        }
     }
 
     file_put_contents(
