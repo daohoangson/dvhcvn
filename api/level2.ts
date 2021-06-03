5dc3ecce387ad514ee8c60209c71b034e8b0f4f4
Dao Hoang Son <daohoangson@gmail.com>
2021-06-03 13:33:10 +0700

    Add support for `gis=yes`

--- api/level2.ts
+++ api/level2.ts
@@ -1,9 +1,11 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 import { data, prepareLevel1, prepareLevel2, prepareLevel3 } from './utils/data';
+import { getGis } from "./utils/vercel";
 
 export default function (req: VercelRequest, res: VercelResponse) {
   const { level1Id, level2Id } = req.query;
+  const gis = getGis(req);
   const level3s = [];
 
   for (const level1 of data) {
@@ -11,12 +13,12 @@ export default function (req: VercelRequest, res: VercelResponse) {
     for (const level2 of level1.level2s) {
       if (level2.level2_id !== level2Id) continue;
       for (const level3 of level2.level3s) {
-        level3s.push(prepareLevel3(level3))
+        level3s.push(prepareLevel3(level3, { level1, level2, req }))
       }
 
       return res.send({
         level1: prepareLevel1(level1),
-        level2: prepareLevel2(level2),
+        level2: prepareLevel2(level2, { gis, level1 }),
         level3s
       });
     }
--- api/utils/data.ts
+++ api/utils/data.ts
@@ -21,35 +21,107 @@ interface Level3 {
   type: string
 }
 
+type Bbox = number[]
+type Coordinates = number[][][]
+interface PreparedData {
+  id: string
+  name: string
+  type: string
+
+  detail: string
+
+  bbox?: Bbox
+  geometry?: {
+    type: string
+    coordinates: Coordinates
+  }
+}
+
+interface GisLevel1 {
+  level1_id: string
+  name: string
+
+  bbox: Bbox
+  type: string
+  coordinates: Coordinates
+
+  level2s: GisLevel2[]
+}
+
+interface GisLevel2 {
+  level2_id: string
+  name: string
+
+  bbox: Bbox
+  type: string
+  coordinates: Coordinates
+}
+
 const dvhcvn = require("../../data/dvhcvn") as { data: Level1[] };
 export const data = dvhcvn.data;
 
+const gis: Record<string, GisLevel1> = {};
+for (const level1 of data) {
+  gis[level1.level1_id] = require(`../../data/gis/${level1.level1_id}.json`);
+}
 
 interface PrepareLevel1Options {
+  gis?: boolean
   req?: VercelRequest
 }
-export function prepareLevel1(level1: Level1, options: PrepareLevel1Options = {}) {
+export function prepareLevel1(level1: Level1, options: PrepareLevel1Options = {}): PreparedData {
   const { level1_id: id, name, type } = level1
   const { req } = options
-  const level2s = req ? `${getSiteUrl(req)}/api/${id}` : undefined
+  const detail = req ? `${getSiteUrl(req)}/api/${id}` : undefined
 
-  return { id, name, type, level2s }
+  let prepared: PreparedData = { id, name, type, detail }
+
+  if (options.gis === true) {
+    const thisGis = gis[id];
+    const { bbox, coordinates, type } = thisGis || {};
+    prepared = { ...prepared, bbox, geometry: { coordinates, type } }
+  }
+
+  return prepared;
 }
 
 interface PrepareLevel2Options {
+  gis?: boolean
   level1?: Level1
   req?: VercelRequest
 }
-export function prepareLevel2(level2: Level2, options: PrepareLevel2Options = {}) {
+export function prepareLevel2(level2: Level2, options: PrepareLevel2Options = {}): PreparedData {
   const { level2_id: id, name, type } = level2
 
   const { level1, req } = options
-  const level3s = (level1 && req) ? `${getSiteUrl(req)}/api/${level1.level1_id}/${id}` : undefined
+  const detail = (level1 && req) ? `${getSiteUrl(req)}/api/${level1.level1_id}/${id}` : undefined
 
-  return { id, name, type, level3s }
+  let prepared: PreparedData = { id, name, type, detail }
+
+  if (level1 && options.gis === true) {
+    const { level2s: gisLevel2s } = gis[level1.level1_id];
+    for (const gisLevel2 of gisLevel2s) {
+      if (gisLevel2.level2_id !== id) continue;
+      const { bbox, coordinates, type } = gisLevel2
+      prepared = { ...prepared, bbox, geometry: { coordinates, type } }
+    }
+  }
+
+  return prepared;
 }
 
-export function prepareLevel3(level3: Level3) {
+interface PrepareLevel3Options {
+  level1?: Level1
+  level2?: Level2
+  req?: VercelRequest
+}
+export function prepareLevel3(level3: Level3, options: PrepareLevel3Options = {}): PreparedData {
   const { level3_id: id, name, type } = level3
-  return { id, name, type }
+
+  const { level1, level2, req } = options
+  const detail = (level1 && level2 && req) ?
+    `${getSiteUrl(req)}/api/${level1.level1_id}/${level2.level2_id}/${id}` :
+    undefined
+
+  return { id, name, type, detail }
 }
