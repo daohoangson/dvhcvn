a07913ccd6582aa4f299c5ca42e7c00b2ec22d9f
Dao Hoang Son <daohoangson@gmail.com>
2023-04-20 14:20:44 +0700

    Replace `@now/node` with Vercel's

--- demo/parser/api/index.ts
+++ demo/parser/api/index.ts
@@ -1,10 +1,10 @@
-import { NowRequest, NowResponse } from "@now/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 import Parser from "../src/parser";
 
 const parser = new Parser();
 
-export default function(req: NowRequest, res: NowResponse) {
+export default function(req: VercelRequest, res: VercelResponse) {
   if (typeof req.body === "string") {
     return res.send(parser.parse(req.body));
   }
