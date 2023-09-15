4cc82e64bffce6a34d492b0b3e0b915bf08c61e0
Dao Hoang Son <daohoangson@gmail.com>
2023-09-15 14:51:27 +0700

    Update format/lint

--- demo/parser/.eslintrc.js
+++ demo/parser/.eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    "prettier",
   ],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module"
-  }
+    sourceType: "module",
+  },
 };
