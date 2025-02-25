7a6e1d08ab9e9f6664787f4be24d2595adae7e0b
Dao Hoang Son <daohoangson@gmail.com>
2025-02-26 01:25:46 +0700

    Deploy with 1GiB to Singapore

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,11 +2,12 @@ import { onSchedule } from "firebase-functions/v2/scheduler";
 import { compareDateValuesBetweenSourceAndRepo } from "./cron";
 import { telegramToken } from "./helpers/telegram";
 
-export const compareDateValuesBetweenSourceAndRepoDailyGen2 = onSchedule(
+export const compareDateValuesBetweenSourceAndRepoDailySingapore = onSchedule(
   {
-    memory: "512MiB",
+    memory: "1GiB",
     schedule: "every day 23:00",
     secrets: [telegramToken],
+    region: "asia-southeast1",
     timeZone: "Asia/Saigon",
   },
   compareDateValuesBetweenSourceAndRepo,
